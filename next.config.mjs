9b0d5938f83b3928c5ee792bb6e7b774e0e1e810
duynguyen222000 <nguyenducduy02022000@gmail.com>
2025-05-20 00:02:11 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export', // BẮT BUỘC khi deploy GitHub Pages
-  // basePath: '/zenchain-article',
-  // assetPrefix: '/zenchain-article/',
-  // images: {
-  //   unoptimized: true, // Để tránh Next xử lý ảnh (vì đang export tĩnh)
-  // },
+  output: 'export', // BẮT BUỘC khi deploy GitHub Pages
+  basePath: '/zenchain-article',
+  assetPrefix: '/zenchain-article/',
+  images: {
+    unoptimized: true, // Để tránh Next xử lý ảnh (vì đang export tĩnh)
+  },
 };
 
 export default nextConfig;
